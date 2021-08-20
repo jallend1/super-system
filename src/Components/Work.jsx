e33df78bed2c9aff92ecf7b714a55a3eacd8ee09
jallend1 <jallend1@gmail.com>
2021-08-19 20:16:47 -0700

    Adds landscape images to projects page and adds animation hover effect

--- src/Components/Work.jsx
+++ src/Components/Work.jsx
@@ -1,12 +1,17 @@
 const Work = () => {
   return (
     <div className="page work">
-      <div>
-        <h1>Here I am!</h1>
-      </div>
-      <div>
-        <div className="contact">Here's another thing!</div>
-      </div>
+      {[1, 2, 3, 4].map((project) => {
+        return (
+          <div className="project">
+            <h2>Project # {project}</h2>
+            <img
+              src={`./images/landscape-${project}.jpg`}
+              alt="Landscaping project"
+            />
+          </div>
+        );
+      })}
     </div>
   );
 };
